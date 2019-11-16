1f498c79a64b5939efd1b1ccfef2da1ab6af527a
shoko <eguchishoko@gmail.com>
2019-11-16 17:14:42 +0900

    [update]testソースに位置情報表示追加して正常動作することを確認

--- app/src/main/java/com/kinsight/test/MapsActivity.kt
+++ app/src/main/java/com/kinsight/test/MapsActivity.kt
@@ -1,5 +1,7 @@
 package com.kinsight.test
 
+import android.Manifest
+import android.annotation.SuppressLint
 import android.content.*
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
@@ -14,9 +16,27 @@ import com.google.android.gms.maps.SupportMapFragment
 import com.google.android.gms.maps.model.LatLng
 import com.google.android.gms.maps.model.MarkerOptions
 
-class MapsActivity : AppCompatActivity(), OnMapReadyCallback {
+// タップしてピン止めし、緯度経度を取得（トースト表示）
+import java.util.Locale
+import java.math.BigDecimal
+import android.content.Context
+import android.content.Intent
+import android.content.pm.PackageManager
+import android.location.LocationListener
+import android.location.LocationManager
+import android.net.Uri
+import android.provider.Settings
+import android.telephony.SmsManager
+import androidx.core.app.ActivityCompat
+import androidx.core.content.ContextCompat
+import android.util.Log
+import android.location.LocationProvider
+import android.location.Location
+
+class MapsActivity : AppCompatActivity(), OnMapReadyCallback, LocationListener {
 
     private lateinit var mMap: GoogleMap
+    private lateinit var locationManager: LocationManager
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -25,6 +45,24 @@ class MapsActivity : AppCompatActivity(), OnMapReadyCallback {
         val mapFragment = supportFragmentManager
             .findFragmentById(R.id.map) as SupportMapFragment
         mapFragment.getMapAsync(this)
+
+        if (ContextCompat.checkSelfPermission(this,
+                Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+            ActivityCompat.requestPermissions(this,
+                arrayOf(Manifest.permission.ACCESS_FINE_LOCATION),
+                1000)
+        } else {
+            locationStart()
+
+            if (::locationManager.isInitialized) {
+                locationManager.requestLocationUpdates(
+                    LocationManager.GPS_PROVIDER,
+                    1000,
+                    50f,
+                    this)
+            }
+
+        }
     }
 
     override fun onResume() {
@@ -65,6 +103,21 @@ class MapsActivity : AppCompatActivity(), OnMapReadyCallback {
         val sydney = LatLng(0.0, 0.0)
         mMap.addMarker(MarkerOptions().position(sydney).title("Marker in Sydney"))
         mMap.moveCamera(CameraUpdateFactory.newLatLng(sydney))
+
+        // タップした時のリスナーをセット
+        mMap.setOnMapClickListener { tapLocation ->
+            // tapされた位置の緯度経度
+            var location = LatLng(tapLocation.latitude, tapLocation.longitude)
+            val str = String.format(Locale.US, "%f, %f", tapLocation.latitude, tapLocation.longitude)
+            mMap.addMarker(MarkerOptions().position(location).title(str))
+            // タップした場所を真ん中に持ってくる
+//            mMap.moveCamera(CameraUpdateFactory.newLatLngZoom(location, 13f))
+            val context = applicationContext
+            val location1 = BigDecimal.valueOf(tapLocation.latitude).toPlainString()
+            val location2 = BigDecimal.valueOf(tapLocation.longitude).toPlainString()
+            val locations = "緯度：$location1　　経度：$location2"
+            Toast.makeText(context, locations, Toast.LENGTH_LONG).show()
+        }
 
         // ダイアログ表示
         dispUserNameDialog();
@@ -109,6 +162,27 @@ class MapsActivity : AppCompatActivity(), OnMapReadyCallback {
         val clipboardManager =
             getSystemService(Context.CLIPBOARD_SERVICE) as ClipboardManager
         clipboardManager.setPrimaryClip(ClipData.newPlainText("label", userName))
+
+        // setContentView(R.layout.activity_maps)
+        // Obtain the SupportMapFragment and get notified when the map is ready to be used.
+
+        if (ContextCompat.checkSelfPermission(this,
+                Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+            ActivityCompat.requestPermissions(this,
+                arrayOf(Manifest.permission.ACCESS_FINE_LOCATION),
+                1000)
+        } else {
+            // locationStart()
+
+            if (::locationManager.isInitialized) {
+                locationManager.requestLocationUpdates(
+                    LocationManager.GPS_PROVIDER,
+                    1000,
+                    50f,
+                    this)
+            }
+
+        }
     }
 
     /**
@@ -119,5 +193,103 @@ class MapsActivity : AppCompatActivity(), OnMapReadyCallback {
         // キャンセルをタップしたときはアプリ終了
         moveTaskToBack(true);
     }
+
+    @SuppressLint("MissingPermission")
+    private fun locationStart() {
+        Log.d("debug", "locationStart()")
+
+        // Instances of LocationManager class must be obtained using Context.getSystemService(Class)
+        locationManager = getSystemService(Context.LOCATION_SERVICE) as LocationManager
+
+        val locationManager = getSystemService(Context.LOCATION_SERVICE) as LocationManager
+
+        if (locationManager.isProviderEnabled(LocationManager.GPS_PROVIDER)) {
+            Log.d("debug", "location manager Enabled")
+        } else {
+            // to prompt setting up GPS
+            val settingsIntent = Intent(Settings.ACTION_LOCATION_SOURCE_SETTINGS)
+            startActivity(settingsIntent)
+            Log.d("debug", "not gpsEnable, startActivity")
+        }
+
+        if (ContextCompat.checkSelfPermission(this,
+                Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+            ActivityCompat.requestPermissions(this,
+                arrayOf(Manifest.permission.ACCESS_FINE_LOCATION), 1000)
+
+            Log.d("debug", "checkSelfPermission false")
+            return
+        }
+        locationManager.requestLocationUpdates(
+            LocationManager.GPS_PROVIDER,
+            1000,
+            50f,
+            this)
+
+    }
+
+    override fun onStatusChanged(provider: String, status: Int, extras: Bundle) {
+        when (status) {
+            LocationProvider.AVAILABLE ->
+                Log.d("debug", "LocationProvider.AVAILABLE")
+            LocationProvider.OUT_OF_SERVICE ->
+                Log.d("debug", "LocationProvider.OUT_OF_SERVICE")
+            LocationProvider.TEMPORARILY_UNAVAILABLE ->
+                Log.d("debug", "LocationProvider.TEMPORARILY_UNAVAILABLE")
+        }
+    }
+
+    override fun onLocationChanged(location: Location) {
+        var loc1 = location.getLatitude()
+        var loc2 = location.getLongitude()
+        var latlng = LatLng(loc1, loc2)
+        val str = String.format(Locale.US, "%f, %f", loc1, loc2)
+        mMap.addMarker(MarkerOptions().position(latlng).title(str))
+        // タップした場所を真ん中に持ってくる
+        mMap.moveCamera(CameraUpdateFactory.newLatLngZoom(latlng, 13f))
+
+        val context = applicationContext
+        val str1 = "Latitude:" + loc1
+        val str2 = "Longtude:" + loc2
+        val str3 = str1 + str2
+        Toast.makeText(context, str3, Toast.LENGTH_LONG).show()
+
+//        val obj = NCMBObject("TestClass")
+//
+//        // 検索
+//        val query = NCMBQuery<NCMBObject>("TestClass")
+//        query.whereEqualTo("objectId", "8mnAQR48G2rdXJjd")
+//        query.findInBackground { results, e ->
+//            if (e != null) {
+//                //検索失敗時の処理
+//            } else {
+//                //検索成功時の処理
+//                var res = results.get(0)
+//                obj.setObjectId(res.objectId)
+//                try {
+//                    var msg = "" + loc1 + ", " + loc2
+//                    obj.put("message", msg)
+//                    obj.saveInBackground { e ->
+//                        if (e != null) {
+//                            //保存に失敗した場合の処理
+//                        } else {
+//                            //保存に成功した場合の処理
+//                        }
+//                    }
+//                } catch (e: NCMBException) {
+//                    e.printStackTrace()
+//                }
+//            }
+//        }
+    }
+
+    override fun onProviderEnabled(provider: String) {
+
+    }
+
+    override fun onProviderDisabled(provider: String) {
+
+    }
+
 
 }
--- app/src/main/java/com/kinsight/test/MeetingMstStore.kt
+++ app/src/main/java/com/kinsight/test/MeetingMstStore.kt
@@ -1,11 +1,11 @@
 package com.kinsight.test
-import com.nifcloud.mbaas.core.NCMB
-import com.nifcloud.mbaas.core.NCMB
+//import com.nifcloud.mbaas.core.NCMB
+//import com.nifcloud.mbaas.core.NCMB
 import com.nifcloud.mbaas.core.NCMBObject
 import com.nifcloud.mbaas.core.NCMBQuery
 
 public class MeetingMstStore(){
-    init {
-        NCMB.initialize(applicationContext, "", "")
-    }
+//    init {
+//        NCMB.initialize(applicationContext, "", "")
+//    }
 }
